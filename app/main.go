193cc8eb8e7b2a0be30d715ed1a31fc10aaa61c1
Jayadeep Bejoy <miniminter2018p@gmail.com>
2026-03-02 03:24:43 +0530

    Multiple pings one connection

--- app/main.go
+++ app/main.go
@@ -29,18 +29,25 @@ func main() {
 		fmt.Println("Connection Accepted")
 
 		bytes := make([]byte, 100)
-		_, err = conn.Read(bytes)
 
-		if err != nil {
-			fmt.Println("Error reading bytes: ", err.Error())
+		for {
+			_, err = conn.Read(bytes)
+
+			if err != nil {
+				fmt.Println("Error reading bytes: ", err.Error())
+				conn.Close()
+				fmt.Println("Connection Closed")
+				break
+			}
+
+			byteString := string(bytes)
+			fmt.Println(byteString)
+
+			// for i := 0; i < len(byteString); i++ {
+			// 	fmt.Printf("%c", byteString[i])
+			// }
+
+			conn.Write([]byte("+PONG\r\n"))
 		}
-
-		// byteString := string(bytes)
-
-		// for i := 0; i < len(byteString); i++ {
-		// 	fmt.Printf("%c", byteString[i])
-		// }
-
-		conn.Write([]byte("+PONG\r\n"))
 	}
 }
