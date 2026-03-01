fb671776d268a34ca8d4afcc323b7319a40ceb9d
Jayadeep Bejoy <miniminter2018p@gmail.com>
2026-03-02 03:06:32 +0530

    PONG cmd

--- app/main.go
+++ app/main.go
@@ -11,15 +11,36 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment the code below to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Connection Accepted")
+
+		bytes := make([]byte, 100)
+		_, err = conn.Read(bytes)
+
+		if err != nil {
+			fmt.Println("Error reading bytes: ", err.Error())
+		}
+
+		// byteString := string(bytes)
+
+		// for i := 0; i < len(byteString); i++ {
+		// 	fmt.Printf("%c", byteString[i])
+		// }
+
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
